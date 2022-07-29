dd5f59ce91cb305b681e608a15078706862d788d
graynet <ryan@grayinternet.com>
2022-07-29 13:46:41 -0500

    Updated wget

--- deployer.sh
+++ deployer.sh
@@ -8,15 +8,18 @@
 # This script sets active interface MTU to 1424
 # sh /usr/bin/herodeploy/deploy.sh
 #
-VERSION=$(date '+%Y-%m-%d %H:%M:%S')
 
-# Check Version
-timeago='30 days ago'
-dtSec=$(date --date "$VERSION" +'%s')
-taSec=$(date --date "$timeago" +'%s')
-[ $dtSec -lt $taSec ] && echo do something
+FILE=/usr/bin/herodeploy/version.txt
+# Check if script has run before
+if test -f "$FILE"; then
+    VERSION=$(cat version.txt)
+    timeago='30 days ago'
+    dtSec=$(date --date "$VERSION" +'%s')
+    taSec=$(date --date "$timeago" +'%s')
+    [ $dtSec -lt $taSec ] && wget https://raw.githubusercontent.com/graynet/managedcloud/master/deployer.sh
+fi
+date '+%Y-%m-%d %H:%M:%S' > version.txt
 
 ip addr show | awk '/inet.*brd/{print $NF; exit}' > interface.txt
 
 for INTERFACE in $(cat interface.txt ) ; do sed -i -e '$aMTU=1424' /etc/sysconfig/network-scripts/ifcfg-${INTERFACE} ; ip link set ${INTERFACE} mtu 1424 ; done ;
-
