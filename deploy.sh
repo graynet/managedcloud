82c8bf705c50d7ecad174d79b1c9295ef2a5c32a
graynet <ryan@grayinternet.com>
2022-07-30 21:29:39 -0500

    Add cronjob

--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,82 @@
 #!/bin/bash
-# Sets active interface MTU
+# Runs on initial boot after cloud-ini scripts
 #
-# Website Shield DDOS Protection
-# MTU Config
+# Hero Deployer Master
+# NameHero Managed Cloud
 # by Namehero.com
 #
-# This script sets active interface MTU to 1376
+# This script sets interface MTU and opens DDOS protected ports
 # sh /usr/bin/herodeploy/deploy.sh
 #
 # Find optimial MTU
 # ping REMOTE_HOSTNAME -c 10 -M do -s 1500
 # where 1500 is mtu then start subtracting by 28
+#
 
+SETMTU=1376
+
+# Version file and general variables
 FILE=/usr/bin/herodeploy/version.txt
-# Check if script has run before
-if test -f "$FILE"; then
-    VERSION=$(cat version.txt)
-    timeago='30 days ago'
-    dtSec=$(date --date "$VERSION" +'%s')
-    taSec=$(date --date "$timeago" +'%s')
-    [ $dtSec -lt $taSec ] && wget https://raw.githubusercontent.com/graynet/managedcloud/master/deployer.sh
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
+LOGFILE=/usr/bin/herodeploy/deploylog.txt
+
+# Start a log file if not one
+if [[ ! -f $LOGFILE ]]
+then
+    touch deploylog.txt
+    echo "$DATE: Log File Created" >> deploylog.txt
 fi
-date '+%Y-%m-%d %H:%M:%S' > version.txt
 
-ip addr show | awk '/inet.*brd/{print $NF; exit}' > interface.txt
-
-for INTERFACE in $(cat interface.txt ) ; do sed -i -e '$aMTU=1376' /etc/sysconfig/network-scripts/ifcfg-${INTERFACE} ; ip link set ${INTERFACE} mtu 1376 ; done ;
-
+# Set cron if script has not run before
 if [[ ! -f $FILE ]]
 then
+    echo "$DATE: Version file not found.  Setting crontab" >> deploylog.txt
+    # Dump existing crons
     crontab -l > cron
+    # Add deploy script 1 time a day M-F
     echo "00 00 * * 1-5 /usr/bin/sh /usr/bin/herodeploy/deploy.sh" >> cron
     crontab cron
     rm cron
+    echo "$DATE: Cronjob has been set 00 00 * * 1-5 /usr/bin/sh /usr/bin/herodeploy/deploy.sh" >> deploylog.txt
+    echo "$DATE" > version.txt
+    echo "$DATE: Version file has been created: $FILE" >> deploylog.txt
 fi
+
+# Check if script needs updated
+# If the version file exists
+if test -f "$FILE"; then
+    # Get current version
+    VERSION=$(cat version.txt)
+    timeago='7 days ago'
+    dtSec=$(date --date "$VERSION" +'%s')
+    taSec=$(date --date "$timeago" +'%s')
+    # If version is older than 7 days download the update script and update the version file.
+    [ $dtSec -lt $taSec ] && wget https://raw.githubusercontent.com/graynet/managedcloud/master/deployer.sh && echo "$DATE" > version.txt
+    echo "$DATE: Version has been updated." >> deploylog.txt
+fi
+
+# Print active interface
+ip addr show | awk '/inet.*brd/{print $NF; exit}' > interface.txt
+ACTIVELINK=$(cat interface.txt)
+echo "$DATE: The current active interface is $ACTIVELINK" >> deploylog.txt
+
+# Set MTU on active interface to 1376 immediately and persist after reboot
+for INTERFACE in $(cat interface.txt) ; do
+  # Set MTU in interface config file.
+  sed -i -e '$aMTU=$SETMTU' /etc/sysconfig/network-scripts/ifcfg-${INTERFACE} ;
+  echo "$DATE: /etc/sysconfig/network-scripts/ifcfg-$INTERFACE has been updated." >> deploylog.txt ;
+  # Set the active link to correct MTU
+  ip link set ${INTERFACE} mtu $SETMTU ;
+  echo "$DATE: The active $INTERFACE has been set to $SETMTU." >> deploylog.txt ;
+done ;
+
+# Open DDOS Protected Ports
+DDOS=/usr/bin/herodeploy/path-whitelist.sh
+
+# If DDOS file detected then run that puppy.
+if test -f "$DDOS"; then
+  echo "$DATE: $DDOS detected.  Running that puppy." >> deploylog.txt ;
+    sh $DDOS
+fi
+
+echo "$DATE: I have reached the end for today.  Goodbye all - drink more caffeine and work harder." >> deploylog.txt ;
--- deployer.sh
+++ deployer.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
-# Sets active interface MTU
+# Checks Managed Cloud VMs for sanity and updates them for protection.
 #
-# Website Shield DDOS Protection
-# MTU Config
-# by Namehero.com
+# Hero Deployer Updater
+# NameHero Managed Cloud
+# by Ryan Gray
 #
-# This script sets active interface MTU to 1424
-# sh /usr/bin/herodeploy/deploy.sh
+# This script checks VPS with NameHero
+# for sanity.  Auto downloads from Git.
 #
-# Find optimial MTU
-# ping REMOTE_HOSTNAME -c 10 -M do -s 1500
-# where 1500 is mtu then start subtracting by 28
+# My name is Ryan Gray
+# I am a hacker CEO
+# I love speed and reliability.
+# But I love your trust.
+# So I am here to keep you safe.  Now drink more caffeine and work harder.
 
-FILE=/usr/bin/herodeploy/version.txt
-# Check if script has run before
-if test -f "$FILE"; then
-    VERSION=$(cat version.txt)
-    timeago='30 days ago'
-    dtSec=$(date --date "$VERSION" +'%s')
-    taSec=$(date --date "$timeago" +'%s')
-    [ $dtSec -lt $taSec ] && wget https://raw.githubusercontent.com/graynet/managedcloud/master/deployer.sh
-fi
-date '+%Y-%m-%d %H:%M:%S' > version.txt
-
-ip addr show | awk '/inet.*brd/{print $NF; exit}' > interface.txt
-
-for INTERFACE in $(cat interface.txt ) ; do sed -i -e '$aMTU=1376' /etc/sysconfig/network-scripts/ifcfg-${INTERFACE} ; ip link set ${INTERFACE} mtu 1376 ; done ;
+echo "$DATE: The Hero Deployer Updater has been completed." >> deploylog.txt ;
